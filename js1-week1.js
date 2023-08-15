b0c9f95a2074c8cf3e7fd5405023990636e5e686
ALEXANDER <msumba83@gmail.com>
2023-08-15 06:38:22 +0200

    done string concatation

--- js1-week1.js
+++ js1-week1.js
@@ -4,3 +4,31 @@ console.log("Hallo, dunial\n");
 console.log("Ciao, mondo\n");
 console.log("Hola, mundo\n");
 
+// VARIABLES 
+const greeting = "Good Morning My Friend";
+console.log(greeting);
+
+// STRINGS TYPEOF
+const colors = "blue, yellow";
+// const colorsTypeOf = typeof colors;
+// console.log(colorsTypeOf);
+console.log(typeof (colors)); //works the same
+
+// String concatenation
+// Here is an example using the plus operator to concat strings
+
+const greetingStart = "Hallo my name is "
+const MyName = "Alexander";
+
+const greetings = greetingStart + MyName
+
+console.log(greetings);
+
+// Here is example using the String interpolation to concat strings
+const startGreeting = "Hello";
+const firstName = "Alexander Junior"
+const secondName = "Msumba"
+
+const fullGreetings = `${startGreeting}, my name is ${firstName} ${secondName}`
+console.log(fullGreetings);
+
